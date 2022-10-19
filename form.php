e27565103335e93bc2a9dd039951472ae58d8870
Martina Joaquin <martujoaquin02@gmail.com>
2022-10-19 12:07:59 -0300

    header y footer index, info

--- form.php
+++ form.php
@@ -5,8 +5,8 @@ $mail = $_POST['mail'];
 $telefono = $_POST['telefono'];
 $mensaje = $_POST['mensaje'];
 
-$para = 'mail';
-$asunto = 'Este email fue enviado desde la web';
+$para = 'lekarimsrl@gmail.com';
+$asunto = 'Este email fue enviado desde la web de Le Karim';
 
 mail($para, $asunto, utf8_decode($nombre,$mail,$telefono,$mensaje));
 
