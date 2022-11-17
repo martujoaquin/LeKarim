b54104e5a75b0e38e2d810488306167fc5b6ecc9
Martina Joaquin <martujoaquin02@gmail.com>
2022-11-17 20:47:00 -0300

    head

--- form.php
+++ form.php
@@ -5,7 +5,7 @@ $mail = $_POST['mail'];
 $telefono = $_POST['telefono'];
 $mensaje = $_POST['mensaje'];
 
-$para = 'lekarimsrl@gmail.com';
+$para = 'martinajoaquin02@gmail.com';
 $asunto = 'Este email fue enviado desde la web de Le Karim';
 
 mail($para, $asunto, utf8_decode($nombre,$mail,$telefono,$mensaje));
